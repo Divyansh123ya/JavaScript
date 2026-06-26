7412b41e85a4c5fc3b718e2dffdad563a27b6132
Divyansh123ya <rajpootdivyansh344@gmail.com>
2026-06-26 05:25:12 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using node.js,not browser
+
+let name="Divyansh"
+let age=18
+let isloggedIn=false
+let state= null
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Divyansh")
+console.log(typeof null)
+console.log(typeof undefined)
