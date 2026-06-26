7b2a96177db79e38c6d15c847e3c801ccb84d5cf
Divyansh123ya <rajpootdivyansh344@gmail.com>
2026-06-26 05:25:43 +0000

    01_variables#

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 41143
+let accountEmail ="divyansh123ya@gmail.com"
+var accountPassword="12345"
+accountCity="Aligarh"
+let accountState
+//accountId=2
+
+accountEmail="rajpootdivyansh344@gmail.com"
+accountPassword="67890"
+accountCity="Chharra"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);
